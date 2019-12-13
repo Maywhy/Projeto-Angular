16f2da4ddd1c7acd5eff70f0211eb1e81baf8e71
Mayara <mayara.cdb@gmail.com>
2019-12-13 16:46:41 -0300

    Primeiro projeto angular

---
+++ primeiro/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
+import { CorpoComponent } from './corpo/corpo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TituloComponent,
+    CorpoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
